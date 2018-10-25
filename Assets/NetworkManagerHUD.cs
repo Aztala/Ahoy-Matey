9bf637640b8741b02d010354be3424e4688fe30e
Aztala <43759776+Aztala@users.noreply.github.com>
2018-10-25 15:01:32 -0400

    added asset, New networking script, Splash screen

---
+++ Assets/NetworkManagerHUD.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+public class NetworkManagerHUD : NetworkManager {
+
+    private GameObject HUDStartHost,HUDStopHost;
+    private void Start()
+    {
+        HUDStartHost = GameObject.Find("StartHost");
+        HUDStopHost = GameObject.Find( "StopHost" );
+        HUDControl( true, false );
+    }
+    public void HostStart()
+    {
+        Debug.Log( Time.timeSinceLevelLoad + " : Starting host");
+        StartHost();
+    }
+    public override void OnStartHost()
+    {
+        Debug.Log( Time.timeSinceLevelLoad + " : Host as started.");
+        HUDControl( false, true );
+        
+    }
+    public override void OnStopHost()
+    {
+        HUDControl( true, false );
+    }
+    public override void OnStartClient(NetworkClient myClient)
+    {
+        Debug.Log( Time.timeSinceLevelLoad + " : client start requested." );
+        InvokeRepeating("DotWait",0f,1f);
+    }
+    public override void OnClientConnect(NetworkConnection conn)
+    {
+        Debug.Log( Time.timeSinceLevelLoad + " : client(" + conn.address + ") is connected");
+        CancelInvoke();
+    }
+    public override void OnClientError(NetworkConnection conn, int errorCode)
+    {
+        Debug.Log( Time.timeSinceLevelLoad + " : client(" + conn.address + ") connection error #" + errorCode);
+    }
+    public override void OnClientDisconnect(NetworkConnection conn)
+    {
+        Debug.Log( Time.timeSinceLevelLoad + " : client(" + conn.address + ") diconnected from server " );
+    }
+
+    private void DotWait()
+    {
+        Debug.Log(".");
+    }
+
+    //Set the display of button
+    private void HUDControl(bool starthost, bool stophost)
+    {
+        HUDStartHost.SetActive( starthost );
+        HUDStopHost.SetActive( stophost );
+    }
+}
---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour {
+
+	float timer = 5f;
+	float elapsed = 0f;
+	bool SoundPlayed = false;
+	AudioSource au;
+	//public LevelManager LM;
+
+
+	// Use this for initialization
+	void Start () {
+		au = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsed += Time.deltaTime;
+
+		if (elapsed >= timer) {
+			
+			//LM.LoadLevel (1);
+			SceneManager.LoadScene (1);
+			//Application.LoadLevel (1); // Old fashion way
+		}
+		else if (elapsed >= timer - 2f && !SoundPlayed)
+		{
+			SoundPlayed = true;
+			au.Play();
+		}
+	}
+}
